540f763e7bc1e5faf67d01ffe4c6ada03c4e84ef
Manish Prajapati <manishprajapat41@gmail.com>
2018-05-26 15:24:16 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Main package function
-function emptyFields(input) {
+function emptyFields(fields) {
     // Returns true if the input is either undefined, null, or empty, false otherwise
     fields.forEach(function(slug) {
         if (slug == undefined || slug == '' || slug == null) { return false; }
