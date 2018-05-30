ac208f413c0dbc72ac61ff15c8b0fc6e7f625e32
Manish Prajapati <manishprajapat41@gmail.com>
2018-05-30 14:31:54 +0530

    Bug fixed

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 // Main package function
 function emptyFields(fields) {
     // Returns true if the input is either undefined, null, or empty, false otherwise
+    temp = false;
     fields.forEach(function(slug) {
-        if (slug == undefined || slug == '' || slug == null) { return false; }
+        console.log(slug);
+        if (slug == undefined || slug == '' || slug == null) {
+            temp = true;
+            return false;
+        }
     });
+    return temp;
 }
 
 // Make the main function available to other packages that require us
